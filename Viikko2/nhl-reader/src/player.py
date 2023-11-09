48174e86db2e9644a879a3e15050588bc885d70c
VP-MaxHax <vp.juselius@gmail.com>
2023-11-09 22:34:31 +0200

    Pelaajat listaan

--- Viikko2/nhl-reader/src/player.py
+++ Viikko2/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
+        self.name = name
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.team = team
+        self.games = games
+        self.penalties = penalties
+
     
     def __str__(self):
         return self.name
